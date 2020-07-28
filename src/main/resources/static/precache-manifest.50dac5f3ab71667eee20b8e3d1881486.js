c01bce75a3fd103b0ff39586ab40f8e5c9810c8f
Jonathan Huertas <jonathanhuertas@Jonathans-MacBook-Pro-2.local>
2020-07-28 14:05:26 -0500

    React

---
+++ src/main/resources/static/precache-manifest.50dac5f3ab71667eee20b8e3d1881486.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6998e4eaeef07b440a13a970ed411a8e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f236501dceb3c32af45",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "a6ef00ccc9f0d7994ee3",
+    "url": "/static/css/main.7556a164.chunk.css"
+  },
+  {
+    "revision": "7f236501dceb3c32af45",
+    "url": "/static/js/2.49984d3d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.49984d3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6ef00ccc9f0d7994ee3",
+    "url": "/static/js/main.9e830550.chunk.js"
+  },
+  {
+    "revision": "dd95a352742441b36716",
+    "url": "/static/js/runtime-main.8c7bb0c5.js"
+  }
+]);
